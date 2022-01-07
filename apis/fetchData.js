af8c66d693d27d6fbc0a567828971a327ccb9650
drakcoder <sairamgundala2000@gmail.com>
2022-01-07 09:15:16 +0530

    first commit

--- apis/fetchData.js
+++ apis/fetchData.js
@@ -4,9 +4,11 @@ const express=require("express");
 fetchDataApiRoute=express.Router();
 
 fetchData=async (req,res)=>{
+    //bringing the database object from local storage
     db=req.app.locals.DatabaseObject;
     // console.log(req.body)
     let query;
+    //checking if equipment_id is given or not and creating query accordingly
     if(req.body.equipment_id==undefined){
         query={
             "project_id":req.body.project_id,
@@ -19,7 +21,9 @@ fetchData=async (req,res)=>{
             "timestamp":{$lte: new Date(req.body.end_date),$gte: new Date(req.body.start_date)}
         }
     }
-    if(req.body.start_date==undefined&&req.body.start_date==undefined){
+    //checkking if date is given in request body
+    if(req.body.start_date==undefined||req.body.start_date==undefined){
+        //setting up date to previous 7 days
         let currDate=new Date();
         let prevDate=new Date(currDate);
         prevDate.setDate(prevDate.getDate()-7);
@@ -27,6 +31,7 @@ fetchData=async (req,res)=>{
         query.timestamp.$lte=currDate;
     }
     // console.log(query);
+    //querying the database
     await db.find(query).toArray((err, docs)=>{
         if(err){
             console.log(err);
